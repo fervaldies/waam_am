339981529a4fe8d9454f60daa35efd45a8906f55
Fernando Valiente Dies <118708559+fervaldies@users.noreply.github.com>
2022-12-01 17:11:51 +1100

    Update main.C

--- src/main.C
+++ src/main.C
@@ -7,7 +7,7 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "c_pfor_amTestApp.h"
+#include "waam_amTestApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
