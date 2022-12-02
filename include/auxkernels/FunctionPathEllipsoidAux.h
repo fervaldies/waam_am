d1a5911b8a6348770bb9302e6dd775f1208c24dc
Fernando Valiente Dies <118708559+fervaldies@users.noreply.github.com>
2022-12-02 11:15:06 +1100

    ScanLengthToVector

--- include/auxkernels/FunctionPathEllipsoidAux.h
+++ include/auxkernels/FunctionPathEllipsoidAux.h
@@ -1,6 +1,6 @@
-// Nicolò Grilli
-// University of Bristol
-// 7 Agosto 2022
+// Fernando Valient Dies & Nicolò Grilli
+// University of Sydney & University of Bristol
+// 29-11-2022
 
 #pragma once
 
@@ -72,7 +72,7 @@ protected:
   
   /// Total time during one scan
   /// After this time the laser is switched off
-  const Real _single_scan_length;
+  const std::vector<Real> _single_scan_length;
   
   /// When the temperature provided by the postprocessor decreases
   /// below this threshold, the heat source is moved to the next
