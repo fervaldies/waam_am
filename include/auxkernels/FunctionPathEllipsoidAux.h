2c72949ec9fd4358fe00a2212b4052d8bb4908c0
Fernando Valiente Dies <118708559+fervaldies@users.noreply.github.com>
2022-12-14 10:44:45 +1100

    Update FunctionPathEllipsoidAux.h

--- include/auxkernels/FunctionPathEllipsoidAux.h
+++ include/auxkernels/FunctionPathEllipsoidAux.h
@@ -25,6 +25,8 @@ public:
 protected:
   virtual Real computeValue() override;
   
+  virtual void checkPPcondition();
+  
   // The default is 0 to 1, but in general the level set will be transformed from
   // _low_level_set_var to _high_level_set_var
   const Real _low_level_set_var;
